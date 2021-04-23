ece0ffbe96cecb3aa78890db1c0f062f5346107c
7x11x13 <gibustf@gmail.com>
2021-04-23 12:58:03 -0400

    Use https API endpoint

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const constants = Object.freeze({
-    API_URL: 'http://ws.audioscrobbler.com/2.0/',
+    API_URL: 'https://ws.audioscrobbler.com/2.0/',
     VALID_USERNAME_REGEX: new RegExp('[\\w-]+'), // old usernames may contain other characters
     TYPE_ARTIST: 'artist',
     TYPE_ALBUM: 'album',
